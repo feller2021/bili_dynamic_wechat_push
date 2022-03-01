5949aaa838206235df9298b15d2cf9fd31eec113
Feller <iamliurenjie@gmail.com>
2022-03-01 17:21:26 +0800

    Update fanfaf.py

--- fanfaf.py
+++ fanfaf.py
@@ -8,7 +8,9 @@ import os
 import json
 from datetime import datetime, timedelta
 
-imgpost = 'https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154'
+imgpost = 'https://push.bot.qw360.cn/room/62b82c00-993b-11ec-9d03-6b5ca40f70fe'
+# 群 https://push.bot.qw360.cn/room/62b82c00-993b-11ec-9d03-6b5ca40f70fe
+# 个人 https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154
 headers = {'Content-Type': 'application/json'}
 
 
