da3102a8ff729dc32f43f8b34e974cf4defff045
Feller <iamliurenjie@gmail.com>
2022-03-26 17:02:05 +0800

    Update fanfaf.py

--- fanfaf.py
+++ fanfaf.py
@@ -8,9 +8,9 @@ import os
 import json
 from datetime import datetime, timedelta
 
-imgpost = 'https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154'
+# imgpost = 'https://push.bot.qw360.cn/send/e54011f0-f9aa-11eb-806f-9354f453c154'
 
-headers = {'Content-Type': 'application/json'}
+# headers = {'Content-Type': 'application/json'}
 
 
 def jiexi(dk, cardss, jk):
@@ -69,13 +69,34 @@ def jiexi(dk, cardss, jk):
         diaoifzf = ifzf.ifzf(orig_dy_id)
         diaoifzf = str(diaoifzf)
         typ = bilitype.bilitype(dynamic_type)
+        # fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + zfnr + '\n' + '------------------------' + '\n' + '------------------------' + '\n' + diaoifzf + '\n' + '------------------------'
 
-        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + zfnr + '\n' + '------------------------' + '\n' + '------------------------' + '\n' + diaoifzf + '\n' + '------------------------'
+        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' '
         print(fasongneir)
         postdata = json.dumps({"msg": fasongneir})
         time.sleep(2)
-        repp = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp)
+        fasongneir2 = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + zfnr + '\n' + '------------------------' + '\n' + '------------------------' + '\n' + diaoifzf + '\n' + '------------------------'
+
+        # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp)
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        HEADERS = {'Content-Type': 'application/json'}
+        FormData = {
+            "appToken": "AT_iaPxpUE0FLNUECu1zFnKhFR7R9NU5K8e",
+            "content": fasongneir2,
+            "summary": fasongneir,
+            "contentType": 2,
+
+            "topicIds": [
+
+            ],
+            "uids": [
+                "UID_noWsar4x3r0zd4WqjCaoD5CIX9Xi"
+            ],
+            "url": ""
+        }
+        res = requests.post(url=url, json=FormData, headers=HEADERS)
+        print(res.text)
 
 
 
@@ -100,22 +121,55 @@ def jiexi(dk, cardss, jk):
         print(dynamic_id)
         typ = bilitype.bilitype(dynamic_type)
 
+        # fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + '图片数量：' + str(
+        #     pictures_count) + ' ' + '张' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        #     neirong) + '\n'
+
         fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + '图片数量：' + str(
+            pictures_count) + ' ' + '张' + '\n'
+
+        print(fasongneir)
+        fasongneir2 = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + '图片数量：' + str(
             pictures_count) + ' ' + '张' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
             neirong) + '\n'
-        print(fasongneir)
-        postdata = json.dumps({"msg": fasongneir})
-        time.sleep(2)
-        repp = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp)
+        # postdata = json.dumps({"msg": fasongneir})
+        # time.sleep(2)
+        # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp)
 
+        # 2022年3月份替换模板消息发送渠道，为了拼接图片。故作出如下更改
+        tupian = ''
         for pic in pic_url:
             zlpic_url = pic['img_src']
 
             print(pic['img_src'])
-            postdata = json.dumps({"msg": {"type": "image", "url": "%s" % zlpic_url}})
-            repp = requests.post(url=imgpost, data=postdata, headers=headers)
-            print(repp)
+            zuhe = "<img src=\"" + zlpic_url + "\" >"
+            tupian += zuhe
+            # postdata = json.dumps({"msg": {"type": "image", "url": "%s" % zlpic_url}})
+            # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+            # print(repp)
+
+        huanghang = "<br />"
+        tupianxianshi = '<meta name="referrer" content="no-referrer" />'
+        content = fasongneir2 + huanghang + tupianxianshi + tupian
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        HEADERS = {'Content-Type': 'application/json'}
+        FormData = {
+            "appToken": "AT_iaPxpUE0FLNUECu1zFnKhFR7R9NU5K8e",
+            "content": content,
+            "summary": fasongneir,
+            "contentType": 2,
+
+            "topicIds": [
+
+            ],
+            "uids": [
+                "UID_noWsar4x3r0zd4WqjCaoD5CIX9Xi"
+            ],
+            "url": ""
+        }
+        res = requests.post(url=url, json=FormData, headers=HEADERS)
+        print(res.text)
 
 
     elif dynamic_type == 4:
@@ -127,13 +181,37 @@ def jiexi(dk, cardss, jk):
         typ = bilitype.bilitype(dynamic_type)
 
         print(wenzi)
-        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        # fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        #     wenzi) + '\n'
+        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n'
+
+        fasongneir2 = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
             wenzi) + '\n'
-        print(fasongneir)
-        postdata = json.dumps({"msg": fasongneir})
-        time.sleep(2)
-        repp = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp)
+        # print(fasongneir)
+        # postdata = json.dumps({"msg": fasongneir})
+        # time.sleep(2)
+        # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp)
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        HEADERS = {'Content-Type': 'application/json'}
+        FormData = {
+            "appToken": "AT_iaPxpUE0FLNUECu1zFnKhFR7R9NU5K8e",
+            "content": fasongneir2,
+            "summary": fasongneir,
+            "contentType": 2,
+
+            "topicIds": [
+
+            ],
+            "uids": [
+                "UID_noWsar4x3r0zd4WqjCaoD5CIX9Xi"
+            ],
+            "url": ""
+        }
+        res = requests.post(url=url, json=FormData, headers=HEADERS)
+        print(res.text)
+
+
 
     elif dynamic_type == 8:
         # 投稿动态
@@ -147,20 +225,46 @@ def jiexi(dk, cardss, jk):
         print(tgtlttle)
         print(tgpic_url)
         typ = bilitype.bilitype(dynamic_type)
-        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        # fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        #     tgtlttle) + '\n'
+        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n'
+
+        fasongneir2 = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
             tgtlttle) + '\n'
         print(fasongneir)
-        postdata = json.dumps({"msg": fasongneir})
-        time.sleep(2)
-        repp = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp)
-        time.sleep(2)
+        # postdata = json.dumps({"msg": fasongneir})
+        # time.sleep(2)
+        # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp)
+        # time.sleep(2)
         # zlpic_url = pic['img_src']
 
         # print(pic['img_src'])
-        postdata36 = json.dumps({"msg": {"type": "image", "url": "%s" % tgpic_url}})
-        repp2 = requests.post(url=imgpost, data=postdata36, headers=headers)
-        print(repp2)
+        # postdata36 = json.dumps({"msg": {"type": "image", "url": "%s" % tgpic_url}})
+        # repp2 = requests.post(url=imgpost, data=postdata36, headers=headers)
+        # print(repp2)
+        huanghang = "<br />"
+        tupianxianshi = '<meta name="referrer" content="no-referrer" />'
+        content = fasongneir2 + huanghang + tupianxianshi + tgpic_url
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        HEADERS = {'Content-Type': 'application/json'}
+        FormData = {
+            "appToken": "AT_iaPxpUE0FLNUECu1zFnKhFR7R9NU5K8e",
+            "content": content,
+            "summary": fasongneir,
+            "contentType": 2,
+
+            "topicIds": [
+
+            ],
+            "uids": [
+                "UID_noWsar4x3r0zd4WqjCaoD5CIX9Xi"
+            ],
+            "url": ""
+        }
+        res = requests.post(url=url, json=FormData, headers=HEADERS)
+        print(res.text)
+
 
 
     elif dynamic_type == 64:
@@ -172,18 +276,43 @@ def jiexi(dk, cardss, jk):
         print(zltitle)
         print(zlpic_url)
         typ = bilitype.bilitype(dynamic_type)
-        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        # fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
+        #     zltitle) + '\n'
+        fasongneir = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n'
+
+        fasongneir2 = '@' + uname + '\n' + dynamic_time + ' ' + '\n' + '▷' + '动态类型：' + typ + ' ' + '\n' + '▷' + '推送时间：' + tzshj + ' ' + '\n' + '▷' + '延时推送：' + timedelay + ' ' + '\n' + '▷' + 'B站链接：' + lj + ' ' + '\n' + '------------------------' + '\n' + str(
             zltitle) + '\n'
         print(fasongneir)
         postdata = json.dumps({"msg": fasongneir})
         time.sleep(2)
-        repp = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp)
-        time.sleep(2)
+        # repp = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp)
+        # time.sleep(2)
         # zlpic_url = pic['img_src']
 
         # print(pic['img_src'])
-        postdata = json.dumps({"msg": {"type": "image", "url": "%s" % zlpic_url}})
-        repp2 = requests.post(url=imgpost, data=postdata, headers=headers)
-        print(repp2)
+        # postdata = json.dumps({"msg": {"type": "image", "url": "%s" % zlpic_url}})
+        # repp2 = requests.post(url=imgpost, data=postdata, headers=headers)
+        # print(repp2)
+        huanghang = "<br />"
+        tupianxianshi = '<meta name="referrer" content="no-referrer" />'
+        content = fasongneir2 + huanghang + tupianxianshi + zlpic_url
+        url = 'http://wxpusher.zjiecode.com/api/send/message'
+        HEADERS = {'Content-Type': 'application/json'}
+        FormData = {
+            "appToken": "AT_iaPxpUE0FLNUECu1zFnKhFR7R9NU5K8e",
+            "content": content,
+            "summary": fasongneir,
+            "contentType": 2,
+
+            "topicIds": [
+
+            ],
+            "uids": [
+                "UID_noWsar4x3r0zd4WqjCaoD5CIX9Xi"
+            ],
+            "url": ""
+        }
+        res = requests.post(url=url, json=FormData, headers=HEADERS)
+        print(res.text)
     print("Write OK")
