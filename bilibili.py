da41be26b1d815d17d5b63482281bc04423875e4
Feller <iamliurenjie@gmail.com>
2022-06-23 09:49:15 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -16,7 +16,7 @@ import os
 class bzMonitor():
     def __init__(self, ):
         self.reqHeaders = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0',
             'Content-Type': 'application/x-www-form-urlencoded',
 
             'Connection': 'close',
